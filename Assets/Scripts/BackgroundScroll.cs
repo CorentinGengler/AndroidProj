6976bede65b69c4201a5dc10c11666e807fdad75
Unknown <neildidi33@gmail.com>
2018-01-04 10:38:30 +0100

    LoopBackground

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float speed;
+
+    void Update ()
+    {
+      Vector2 offset = new Vector2(0,Time.time * speed );
+      GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }﻿
+}
